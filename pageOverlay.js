e1f80798a45e969d4b1e8d49fd16adc8e2f1e703
chromakode <c@chromakode.com>
2010-04-25 00:16:47 -0700

    Working voting, info updating; improved scraping.

--- pageOverlay.js
+++ pageOverlay.js
@@ -6,7 +6,7 @@ shineOverlay = {
   init: function() {
 
     var result = $('<div id="_shine-overlay"></div>')
-      .append($('<iframe id="_shine-frame" src="" scrolling="no" frameborder="0"></iframe>'))
+      .append($('<iframe id="_shine-frame" src="bar.html" scrolling="no" frameborder="0"></iframe>'))
       .appendTo('body')
       .hide();
 
@@ -15,9 +15,15 @@ shineOverlay = {
 
   display: function(info) {
     if (info) {
-      if (!this.info || this.info.fullname != info.fullname) {
+      if (!this.info || this.info.fullname == info.fullname) {
         this.info = info;
+        // Another chrome bug prevents us from properly using postMessage on our child iframe, so we resort to another hack:
+        // We'll change the height of the iframe ever so slightly (not displayed), and pick up the resize event inside the frame.
+        console.log(chrome.extension.getURL('bar.html#'+encodeURIComponent(JSON.stringify(info))));
         $('#_shine-overlay iframe').show().attr('src', chrome.extension.getURL('bar.html#'+encodeURIComponent(JSON.stringify(info))));
+        window.setTimeout(function() {
+          $('#_shine-overlay iframe').height($('#_shine-overlay iframe').height()+1);
+        }, 0);
       }
     } else {
       this.info = null;
@@ -42,14 +48,16 @@ shineOverlay = {
 }
 
 function onRequest(request, sender, callback) {
-  if (request.action == 'showOverlay') {
+  if (request.action == 'showInfo') {
+    console.log('Shine showInfo update received:', request.info);
     shineOverlay.display(request.info);
     shineOverlay.show();
   }
 }
 chrome.extension.onRequest.addListener(onRequest);
 
-
+// Unfortunately, chrome.* is sadly not available to iframes in current versions.
+// We'll have to get a little bit creative!
 function receiveMessage(event) {
   if (event.origin == chrome.extension.getURL('').slice(0, -1)) {
     var request = JSON.parse(event.data);
@@ -59,6 +67,9 @@ function receiveMessage(event) {
       $('#_shine-overlay').height(request.height+'px');
     } else if (request.action == 'close') {
       shineOverlay.hide();
+    } else if (request.action == 'vote') {
+      request.fullname = shineOverlay.info.name;
+	    chrome.extension.sendRequest(request);
     }
   }
 }
--- redditContent.js
+++ redditContent.js
@@ -1,12 +1,41 @@
-function thingClicked() {
-	var fullnameMatch = $(this).parents('.thing').attr('class').match(/id-(\w+)/);
-	if (fullnameMatch) {
-		var info = {fullname:fullnameMatch[1]};
-		info.title = $(this).text();
-	  chrome.extension.sendRequest({action:'thingClick', url:this.href, info:info});
+function scrapeThingInfo(thing) {
+  var info = {};
+
+	fullnameMatch = thing.attr('class').match(/id-(\w+)/);
+	if (!fullnameMatch) {
+	  return false;
 	}
+	info.name = fullnameMatch[1];
+	
+	info.title = thing.find('a.title').text();
+
+  var entry = thing.find(".entry");
+  if (entry.hasClass('likes')) {
+    info.likes = true;
+  } else if (entry.hasClass('dislikes')) {
+    info.likes = false;
+  } else {
+    info.likes = null;
+  }
+
+  info.score = parseInt(thing.find(".score").text());
+
+  info.num_comments = parseInt(thing.find('.comments').text());
+
+  info.permalink = thing.find('.comments').attr('href').match(/.*reddit.com(\/.+)/)[1];
+
+  console.log('Scraped info from page:', info);
+  return info;
+}
+
+function thingClicked() {
+  var info = scrapeThingInfo($(this).parents('.thing'));
+  if (info) {
+    chrome.extension.sendRequest({action:'thingClick', url:this.href, info:info});
+  }
 }
 
 $(document).ready(function() {
+  console.log('Shine reddit content handler running.');
 	$('.thing a.title').click(thingClicked);
 });
