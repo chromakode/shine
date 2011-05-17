b5ddf9666edecb78717070e24a2128399b2edd08
Andre D <andre@andred.ca>
2011-05-17 17:31:14 -0400

    Added logic to verify that a bar does not already exist before adding one

--- pageOverlay.js
+++ pageOverlay.js
@@ -44,35 +44,6 @@ ShineOverlay.prototype = {
   }
 }
 
-
-shineBar = new ShineOverlay('bar');
-$.extend(shineBar, {
-  _display: function(url) {
-    this.frame.show().attr('src', chrome.extension.getURL(url));
-    window.setTimeout($.proxy(function() {
-      this.frame.height(this.frame.height()+1);
-    }, this), 10);
-  },
-
-  display: function(info, loggedIn) {
-    if (info) {
-      if (!this.info || this.info.name == info.name) {
-        this.info = info;
-        // Another chrome bug prevents us from properly using postMessage on our child iframe, so we resort to another hack:
-        // We'll change the height of the iframe ever so slightly (not displayed), and pick up the resize event inside the frame.
-        this._display('bar.html#'+encodeURIComponent(JSON.stringify({info:info, loggedIn:loggedIn})));
-      }
-    } else {
-      this.info = null;
-      this.frame.hide();
-    }
-  },
-
-  showSubmit: function() {
-    this._display('submit.html#'+encodeURIComponent(window.location.href));
-  }
-});
-
 function onRequest(request, sender, callback) {
   if (request.action == 'showInfo') {
     console.log('Shine showInfo update received:', request.info);
@@ -81,7 +52,6 @@ function onRequest(request, sender, callback) {
     shineBar.showSubmit();
   }
 }
-chrome.extension.onRequest.addListener(onRequest);
 
 // Unfortunately, chrome.* is sadly not available to iframes in current versions.
 // We'll have to get a little bit creative!
@@ -100,6 +70,40 @@ function receiveMessage(event) {
     }
   }
 }
-window.addEventListener('message', receiveMessage, false);
 
-console.log('Shine page overlay loaded.');
+function addBar(){
+  shineBar = new ShineOverlay('bar');
+  $.extend(shineBar, {
+    _display: function(url) {
+      this.frame.show().attr('src', chrome.extension.getURL(url));
+      window.setTimeout($.proxy(function() {
+        this.frame.height(this.frame.height()+1);
+      }, this), 10);
+    },
+
+    display: function(info, loggedIn) {
+      if (info) {
+        if (!this.info || this.info.name == info.name) {
+          this.info = info;
+          // Another chrome bug prevents us from properly using postMessage on our child iframe, so we resort to another hack:
+          // We'll change the height of the iframe ever so slightly (not displayed), and pick up the resize event inside the frame.
+          this._display('bar.html#'+encodeURIComponent(JSON.stringify({info:info, loggedIn:loggedIn})));
+        }
+      } else {
+        this.info = null;
+        this.frame.hide();
+      }
+    },
+
+    showSubmit: function() {
+      this._display('submit.html#'+encodeURIComponent(window.location.href));
+    }
+  });
+  chrome.extension.onRequest.addListener(onRequest);
+  window.addEventListener('message', receiveMessage, false);
+  console.log('Shine page overlay loaded.');
+}
+
+if($('._shine-overlay').size()==0){
+  addBar();
+}
