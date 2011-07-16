1f10ec7665cc6b952aab3fe02598eca2ca13a6ee
Max Goodman <c@chromakode.com>
2011-07-16 00:55:47 -0700

    Fix logic error in options page initialization.

--- src/options.js
+++ src/options.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  if (localStorage['showTooltips']) { $('#showTooltips').prop('checked', true) }
+  if (localStorage['showTooltips'] == 'true') { $('#showTooltips').prop('checked', true) }
   $('input[type=checkbox]').click(function() {
     localStorage[$(this).attr('id')] = $(this).is(':checked')
   })
