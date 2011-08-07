4213f2436cd64339f4c0fd53bacef50645058777
mhweaver <mhweaver@gmail.com>
2011-08-07 10:32:38 +0300

    Fix bug where clicking thumbnails would not cause the bar to open.

--- src/redditContent.js
+++ src/redditContent.js
@@ -43,8 +43,9 @@ function scrapeThingInfo(thing) {
 
 function thingClicked(e) {
   var a = e.target
-  if (a.nodeName != 'A' || !a.classList.contains('title')) { return }
-    
+  if (a.nodeName == 'IMG') { a = a.parentElement }
+  if (a.nodeName != 'A' || (!a.classList.contains('title') && !a.classList.contains('thumbnail'))) { return }
+
   // Find the parent element of the clicked link that represents the entire thing.
   var el = a
   do {
