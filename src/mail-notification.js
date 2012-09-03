bbf567ffcd6dae439e73b692cb13007f36690ce0
Max Goodman <c@chromakode.com>
2012-09-03 16:47:22 -0700

    Clean up mail-notification.js.

--- src/mail-notification.js
+++ src/mail-notification.js
@@ -1,15 +1,13 @@
+function expand(text) {
+  return unescape(text).replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&')
+}
+
 $(document).ready(function() {
   var data = JSON.parse(window.location.hash.substr(1))
 
-  window.scroll(0, 0)
-
   console.log('Received message data:', data)
 
-  function expand(text) {
-    return unescape(text).replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&')
-  }
-
-  $('#mail-image').attr('src', expand(data.image))
+  $('#mail-image').attr('src', data.image)
   $('#from').html(data.message.author)
   if (data.message.subreddit) {
     $('#r').html('/r/'+data.message.subreddit).addClass('sr')
